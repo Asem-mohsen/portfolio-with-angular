645e6795f64cfc9aedd6aa0e3cc8abb13a4d81a2
Assem <asemmohsen911@gmail.com>
2024-08-03 19:30:00 +0300

    new

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import { portfolio } from '../../interface/portfolio.interface';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.css'
 })
+
 export class PortfolioComponent {
 
+  portfolioCards:portfolio[]=[
+    {img:'../../assets/imgs/poert1.png'},
+    {img:'../../assets/imgs/port2.png'},
+    {img:'../../assets/imgs/port3.png' },
+    {img:'../../assets/imgs/poert1.png'},
+    {img:'../../assets/imgs/port2.png'},
+    {img:'../../assets/imgs/port3.png'},
+  ];
+
+  isModalOpen = false;
+  selectedImage: string | null = null;
+
+  openModal(imageSrc: string) {
+    this.selectedImage = imageSrc;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
+    this.selectedImage = null;
+  }
 }
---
+++ src/interface/portfolio.interface.ts
@@ -0,0 +1,3 @@
+export interface portfolio{
+  img:string,
+}
