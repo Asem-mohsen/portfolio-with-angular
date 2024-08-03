38f804f8242a11ff477e22f602e73c0fd223de89
Assem <asemmohsen911@gmail.com>
2024-08-03 18:52:45 +0300

    new

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.css'
+})
+export class AboutComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { ContactComponent } from "./contact/contact.component";
+import { FooterComponent } from "./footer/footer.component";
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent , ContactComponent , AboutComponent , HomeComponent, PortfolioComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Portofolio';
+  title = 'Portfolio';
 }
